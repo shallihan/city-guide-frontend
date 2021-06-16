1d5b5c1187ab404249871401ea700f744ebcafea
shallihan <smhallihan@gmail.com>
2021-06-16 15:55:15 +0100

    Setting up custom inputs with validation for use in any forms.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,10 @@
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import './NewPlace.css';
 
 const NewPlace = () => {
     return <form className="place-form">
-        <Input type="text" label="title" />
+        <Input type="text" label="Title" element="input" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title" />
     </form>
 };
 
