31df465dbb3f3c8274ab0d59506c7264a04e1211
shallihan <smhallihan@gmail.com>
2021-06-16 09:58:25 +0100

    Adding Styling to UserItem

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,16 +1,24 @@
+import { Link } from "react-router-dom";
 import "./UserItem.css";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
+
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="users-item__content">
-        <div className="users-item__image">
-          <img src={props.image} alt={props.name} />
-        </div>
-        <div className="users-item__info">
+      <Card className="users-item__content">
+        <Link to={`/${props.id}/`}>
+          <div className="users-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="users-item__info">
             <h2>{props.name}</h2>
-            <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
-        </div>
-      </div>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,15 +1,16 @@
-import UsersList from "../components/UsersList";
+import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
     {
-      id: 45855,
-      name: "Yuna Olga",
+      id: 'u1',
+      name: 'Max Schwarz',
       image:
-        "https://images.unsplash.com/photo-1418854982207-12f710b74003?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      places: 3,
-    },
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
   ];
+
   return <UsersList items={USERS} />;
 };
 
