5a237df374d6b0ca1f1697ad3aea2fffd9646c55
shallihan <smhallihan@gmail.com>
2021-06-16 21:54:57 +0100

    Adding Login Form

--- src/user/pages/Authenticate.js
+++ src/user/pages/Authenticate.js
@@ -1,13 +1,60 @@
-import './Authenticate.css';
-import Card from '../../shared/components/UIElements/Card';
+import "./Authenticate.css";
+import Card from "../../shared/components/UIElements/Card";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import { useForm } from "../../shared/hooks/form-hook";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
 
 const Authenticate = () => {
-    return (
-        <Card>
-        <form></form>
-        </Card>
-    );
+  const [formState, inputHandler] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+  const loginSubmitHandler = (event) => {
+    event.preventDafault();
+    console.log(formState.inputs);
+  };
+  return (
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={loginSubmitHandler}>
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="E-Mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address."
+          onInput={inputHandler}
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(6)]}
+          errorText="Please enter a valid password. Must be at least 6 characters long."
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </Card>
+  );
 };
-
 
 export default Authenticate;
