7340c663c3e8008f78064250e819d4470e1a0ea3
shallihan <smhallihan@gmail.com>
2021-06-16 11:06:56 +0100

    Added Navigation SideDrawer state and functionality.

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,33 +1,44 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 import MainHeader from "./MainHeader";
 import NavigationLinks from "./NavigationLinks";
 import "./MainNavigation.css";
-import React from "react";
-
 
 const MainNavigation = (props) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const openDrawer = () => {
+    setDrawerOpen(true);
+  };
+  const closeDrawer = () => {
+    setDrawerOpen(false);
+  }
   return (
-    <React.Fragment>
-    <SideDrawer>
-      <nav className="main-navigation__drawer-nav">
-        <NavigationLinks />
-      </nav>
-    </SideDrawer>
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavigationLinks />
-      </nav>
-    </MainHeader>
-    </React.Fragment>
+    <>
+      {drawerOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerOpen &&
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavigationLinks />
+          </nav>
+        </SideDrawer>
+      }
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavigationLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 };
 
--- src/shared/components/UIElements/Avatar.js
+++ src/shared/components/UIElements/Avatar.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import './Avatar.css';
 
 const Avatar = (props) => {
