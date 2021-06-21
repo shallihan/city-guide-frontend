efae2c4045c6015ebe026af41a53242bada9c260
shallihan <smhallihan@gmail.com>
2021-06-21 16:29:15 +0100

    Adding a fetch method to call api endpoint which adds a user to the MongoDB database.

--- src/user/pages/Authenticate.js
+++ src/user/pages/Authenticate.js
@@ -50,9 +50,28 @@ const Auth = () => {
     }
     setIsLoginMode((prevMode) => !prevMode);
   };
-  const loginSubmitHandler = (event) => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
+    if (isLoginMode) {
+    } else {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
     authorisation.login();
   };
 
@@ -60,7 +79,7 @@ const Auth = () => {
     <Card className="authentication">
       <h2>Login Required</h2>
       <hr />
-      <form onSubmit={loginSubmitHandler}>
+      <form onSubmit={authSubmitHandler}>
         {!isLoginMode && (
           <Input
             element="input"
